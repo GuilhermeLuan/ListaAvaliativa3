e06f9bc8a78f3cb01b057708cb0681322b8c21c8
Guilherme Luan <guilhermeluan@pm.me>
2023-11-08 19:44:06 -0300

    feat: tratamento placa

--- Q3.c
+++ Q3.c
@@ -10,45 +10,76 @@ void toLowerCase(char *str) {
 }
 
 int main(){
-    char placaCarro[9] = "AAA-1010";
-    char diaSemana[14] = "DOMINGO";
+    char placaCarro[9] = "XX-1111";
+    char diaSemana[14] = "TERCA-FEIRA";
     char diaSemanaNaoPermitido[14];
     char *diasDaSemana[] = {
             "DOMINGO", "SEGUNDA-FEIRA", "TERÇA-FEIRA",
-            "QUARTA-FEIRA", "QUINTA-FEIRA", "SEXTA-FEIRA", "SÁBADO"
+            "QUARTA-FEIRA", "QUINTA-FEIRA", "SEXTA-FEIRA", "SABADO"
     };
     char *contemHifem = strchr(placaCarro, '-');
-    int ultimoDigitoPlaca = placaCarro[strlen(placaCarro) - 1] - '0', count = 0;
-
-    //Validacao placa
-        //Validacao das placas antigas
-    if(contemHifem != NULL){
-
-    }
-    printf("%d", count);
+    int ultimoDigitoPlaca = placaCarro[strlen(placaCarro) - 1] - '0', qtdLetras = 0, qtdNumeros = 0;
+    int placaEhValida, diaEhValido;
 
 
 
-    if(ultimoDigitoPlaca == 0 || ultimoDigitoPlaca == 1){
-        strcpy(diaSemanaNaoPermitido, "SEGUNDA-FEIRA");
-    }else if(ultimoDigitoPlaca == 2 || ultimoDigitoPlaca == 3){
-        strcpy(diaSemanaNaoPermitido, "TERCA-FEIRA");
-    }else if(ultimoDigitoPlaca == 4 || ultimoDigitoPlaca == 5){
-        strcpy(diaSemanaNaoPermitido, "QUARTA-FEIRA");
-    }else if(ultimoDigitoPlaca == 6 || ultimoDigitoPlaca == 7){
-        strcpy(diaSemanaNaoPermitido, "QUINTA-FEIRA");
-    }else if(ultimoDigitoPlaca == 8 || ultimoDigitoPlaca == 9){
-        strcpy(diaSemanaNaoPermitido, "SEXTA-FEIRA");
+    //Conta a qtd de letras e numeros da Placa
+    for (int i = 0; i < strlen(placaCarro); ++i) {
+        if(isalpha(placaCarro[i])){
+            qtdLetras++;
+        } if(isdigit(placaCarro[i])){
+            qtdNumeros++;
+        }
     }
 
-    if (strcmp(diaSemana, "SABADO") == 0 || strcmp(diaSemana, "DOMINGO") == 0){
-        printf("Nao ha proibicao no fim de semana");
+    //Valida a placa
+    if(contemHifem != NULL){ //placa antiga
+        if(qtdLetras != 3 || qtdNumeros != 4){
+            printf("Placa invalida\n");
+        }
+    } else{ // placa nova
+        if(qtdLetras != 4 || qtdNumeros != 3){
+            printf("Placa invalida\n");
+        }
+    }
+
+    for (int i = 0; i < 6; i++) {
+        if (strstr(diaSemana, diasDaSemana[i]) != NULL) {
+            switch (ultimoDigitoPlaca) {
+        case 0:
+        case 1:
+            strcpy(diaSemanaNaoPermitido, diasDaSemana[1]);
+            break;
+        case 2:
+        case 3:
+            strcpy(diaSemanaNaoPermitido, diasDaSemana[2]);
+            break;
+        case 4:
+        case 5:
+            strcpy(diaSemanaNaoPermitido, diasDaSemana[3]);
+            break;
+        case 6:
+        case 7:
+            strcpy(diaSemanaNaoPermitido, diasDaSemana[4]);
+            break;
+        case 8:
+        case 9:
+            strcpy(diaSemanaNaoPermitido, diasDaSemana[5]);
+            break;}
+        } else if (strstr(diaSemana, diasDaSemana[i]) != NULL){
+            printf("Dia da semana invalido\n");
+            return 0;
+        }
+    }
+
+    if (strcmp(diaSemana, diasDaSemana[0]) == 0 || strcmp(diaSemana, diasDaSemana[6]) == 0){
+        printf("Nao ha proibicao no fim de semana\n");
     } else if(strcmp(diaSemana, diaSemanaNaoPermitido) != 0){
         toLowerCase(diaSemana);
-        printf("%s pode circular %s", placaCarro, diaSemana);
+        printf("%s pode circular %s\n", placaCarro, diaSemana);
     } else{
         toLowerCase(diaSemana);
-        printf("%s nao pode circular %s", placaCarro, diaSemana);
+        printf("%s nao pode circular %s\n", placaCarro, diaSemana);
 
     }
     return 0;
